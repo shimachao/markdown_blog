ca06340ba82e3561d6053b12337c87f5bcdaf6a7
chao <531236305@qq.com>
2016-07-13 22:33:13 +0800

    添加404页面，以及在请求不存在的文章时返回404

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, abort
 from db import Article
 from md_renderer import md
 from flask.ext.bootstrap import Bootstrap
@@ -8,14 +8,24 @@ from flask.ext.bootstrap import Bootstrap
 app = Flask(__name__)
 boot = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def index():
     return '<h1>正在施工中...</h1><h1>施工完会看到文章列表</h1>'
 
+
 @app.route('/article/<int:article_id>')
 def article(article_id):
     # 返回请求的文章
-    d = Article.select().where(Article.id == article_id).get()
+    d = Article.select().where(Article.id == article_id)
+    print(d)
+    if not d:
+        abort(404)
+    d = d.get()
     path = '..\\' + d.path
     file = open(file=path, mode='r', encoding='utf-8')
     text = md(file.read())
